4df1f70acb25be3d5cbf30eee3250198eede59b9
Ali Othman <alioth840@gmail.com>
2024-06-07 15:18:11 +0200

    I have created Home component and I modified the app.js file

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
