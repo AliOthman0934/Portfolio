6a69619cc95def1488e52d9823ca3620188a0d39
Ali Othman <alioth840@gmail.com>
2024-06-27 07:44:46 +0200

    I have created taimeline for the education and skills

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from 'react'
 import Footer from '../Footer/Footer'
 import GetInTouch from '../GetInTouch/GetInTouch'
 import Header from '../Header/Header'
@@ -6,6 +6,7 @@ import './About.css'
 import gsap from 'gsap'
 import flower from '../../Assets/svgviewer-output.svg'
 import Resume from '../Resume/Resume'
+import Strength from '../Strength/Strength'
 function About() {
     let text1 = useRef(null)
     let text2 = useRef(null)
@@ -15,7 +16,7 @@ function About() {
     useEffect(() => {
         timeline_about.from([text1, text2], {
             duration: .8,
-            y: 500,
+            y: 600,
             skewY: 10,
             stagger: {
                 amount: .4
@@ -36,71 +37,74 @@ function About() {
     })
     return (
         <>
-        <div>
-            <div className="about">
-                <Header timeline={timeline_about}/>
-                <div className="about-container">
-                    <div className="container-text">
-                        <div className="container-inner-text1">
-                            <p ref={el => text1 = el}>
-                                Ali
+            <div>
+                <div className="about">
+                    <Header timeline={timeline_about} />
+                    <div className="about-container">
+                        <div className="container-text">
+                            <div className="container-inner-text1">
+                                <p ref={el => text1 = el}>
+                                    Ali
+                                </p>
+                            </div>
+                            <div className="container-inner-text2">
+                                <p ref={el => text2 = el}>
+                                    Othman
+                                </p>
+                            </div>
+                        </div>
+                        <di ref={el => p1 = el} v className="container-quote container-quote1">
+                            I create sites and <br /> applications that cause <br /> pleasant emotions
+                        </di>
+                        <div className="container-quote container-quote2">
+                            I specialize in interaction <br /> design, experience websites <br /> and applications
+                        </div>
+                        <div className="container-image" ref={el => image = el}></div>
+                    </div>
+                    <div className="about-container2">
+                        <div className="about-container2-heading">
+                            <div className="about-text-lower">
+                                <p>Full Stack</p>
+                            </div>
+
+                            <div className="about-text-lower">
+                                <p>&nbsp;Web Developer&nbsp;&nbsp;&</p>
+                            </div>
+                            <div className="about-text-lower">
+                                <p>Architecture Enthusiast</p>
+                            </div>
+                            <div className="about-container-flower">
+                            <img src={flower} alt="" />
+                        </div>
+                        </div>
+
+                        <div className="resume-container">
+                            <Resume />
+                        </div>
+
+                        {/* <div className="about-container-my-strength">
+                            <div className="about-text-lower">
+                                <p>My Strengths</p>
+                            </div>
+                        </div>
+                        <div className="about-container2-strength-p">
+                            <p className="about-container2-p-right">
+                                I BELIEVE THAT EVERY PROJECT THAT I DO SHOULD HAVE AN OVERVALUE.<br />I ALWAYS TRY TO FIND THE OPTIMAL SOLUTION TO THE CLIENT'S TASK.
                             </p>
                         </div>
-                        <div className="container-inner-text2">
-                            <p ref={el => text2 = el}>
-                                Othman
-                            </p>
-                        </div>
-                    </div>
-                    <di  ref={el => p1 = el}v className="container-quote container-quote1">
-                        I create sites and <br /> applications that cause <br /> pleasant emotions
-                    </di>
-                    <div className="container-quote container-quote2">
-                        I specialize in interaction <br /> design, experience websites <br /> and applications
-                    </div>
-                    <div className="container-image"  ref={el => image = el}></div>
-                </div>
-                <div className="about-container2">
-                    <div className="about-container2-heading">
-                        <div className="about-text-lower">
-                            <p>Full Stack</p>
-                        </div>
-
-                        <div className="about-text-lower">
-                            <p>&nbsp;Web Developer&nbsp;&nbsp;&</p>
-                        </div>
-                        <div className="about-text-lower">
-                            <p>Architecture Enthusiast</p>
-                        </div>
+                        <div className="about-container-flower">
+                            <img src={flower} alt="" />
+                        </div> */}
                     </div>
 
-                    
-
-                    <div className="about-container2-p">
-                        <Resume/>
+                    <div className="about-footer">
+                        <Strength/>
+                        <GetInTouch />
+                        <Footer />
                     </div>
-                    <div className="about-container-my-strength">
-                        <div className="about-text-lower">
-                            <p>My Strengths</p>
-                        </div>
-                    </div>
-                    <div className="about-container2-strength-p">
-                        <p className="about-container2-p-right">
-                        I BELIEVE THAT EVERY PROJECT THAT I DO SHOULD HAVE AN OVERVALUE.<br />I ALWAYS TRY TO FIND THE OPTIMAL SOLUTION TO THE CLIENT'S TASK.
-                        </p>
-                        
-                    </div>
-                    <div className="about-container-flower">
-                        <img src={flower} alt="" />
-                    </div>
-                </div>
-                <div className="about-footer">
-                    <GetInTouch />
-                    <Footer />
                 </div>
             </div>
-        </div>
-        
+
         </>
     )
 }
